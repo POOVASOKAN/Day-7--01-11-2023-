39faded7e16b1ac63f0822be483e106fb5a35547
poovasokan <poovasokan@gmail.com>
2023-11-01 17:58:27 +0000

    Updated script.js to fix issue with JSON

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 //A) Get all the countries from Asia continent /region using Filter function
-var request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all", true);
-request.send();
-request.onload = function () {
-    var data = request.response;
-    var result = JSON.parse(data);
-    var asianCountries = result.filter((country) => {
-        return country.region === 'Asia';
+var request1 = new XMLHttpRequest();
+request1.open("GET", "https://restcountries.com/v3.1/all", true);
+request1.send();
+request1.onload = function () 
+{
+    var data1 = request1.response;
+    var results = JSON.parse(data1);
+    var asianCountries = results.filter((country1) => {
+        return country1.region === 'Asia';
     });
     var asianCountryNames = asianCountries.map((country) => country.name.common);
     console.log(asianCountryNames);
@@ -16,14 +17,14 @@ request.onload = function () {
 
 
 //B) Get all the countries with a population of less than 2 lakhs using Filter function
-var request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all", true);
-request.send();
-request.onload = function () {
-    var data = request.response;
-    var result = JSON.parse(data);
-    var countriesWithLowPopulation = result.filter((country) => {
-        return country.population < 200000;
+var request2 = new XMLHttpRequest();
+request2.open("GET", "https://restcountries.com/v3.1/all", true);
+request2.send();
+request2.onload = function () {
+    var data2 = request2.response;
+    var result2 = JSON.parse(data2);
+    var countriesWithLowPopulation = result2.filter((country2) => {
+        return country2.population < 200000;
     });
     var countryNames = countriesWithLowPopulation.map((country) => country.name.common);
     console.log(countryNames);
@@ -32,13 +33,13 @@ request.onload = function () {
 
 
 //C) Print the following details name, capital, flag, using forEach function
-var request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/all", true);
-request.send();
-request.onload = function () {
-    var data = request.response;
-    var result = JSON.parse(data);
-    result.forEach((country) => {
+var request3 = new XMLHttpRequest();
+request3.open("GET", "https://restcountries.com/v3.1/all", true);
+request3.send();
+request3.onload = function () {
+    var data3 = request3.response;
+    var result4 = JSON.parse(data3);
+    result4.forEach((country) => {
         var name = country.name.common;
         var capital = Array.isArray(country.capital) ? country.capital[0] : "N/A";  
         var flag = country.flags.png; 
